1727eee6db003a03add4c65efc1b25abd6a3a867
B1s0m <b0b.30916@gmail.com>
2026-07-16 16:41:32 +0300

    add create prodewct page

--- controllers/employee.controllers.js
+++ controllers/employee.controllers.js
@@ -1,7 +1,16 @@
 const router = require("express").Router()
-const employee=require("./../models/Product")
+const Products=require("./../models/Product")
 
 router.get('/new',(req,res)=>{
     res.render('employee/createProducts.ejs')
 })
+router.post('/new', async(req,res)=>{
+    console.log(req.session.user._id)
+    req.body.createdBy=req.session.user._id;
+    console.log(req.body)
+    //   const addProducts= await Products.create(req.body)
+    res.redirect("/products/new")
+})
+
+
 module.exports = router;
